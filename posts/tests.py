c46a4b373e79e21c388d88af4ab56e8c44a51ad2
abak-sale <abaksale2@gmail.com>
2026-06-11 16:43:16 +0700

    Sudah selesai bab 5 menampilkan data ke web dan pengujian test.py

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Post
 
-# Create your tests here.
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Membuat data tiruan di database khusus testing
+        cls.post = Post.objects.create(text="Ini tes pesan")
+
+    def test_model_content(self):
+        # Menguji apakah isi database sesuai
+        self.assertEqual(self.post.text, "Ini tes pesan")
+
+    def test_url_exists_at_correct_location(self):
+        # Menguji apakah URL '/' bisa diakses (Status 200)
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        # Menguji apakah template yang dipakai sudah benar
+        response = self.client.get(reverse("posts"))
+        self.assertTemplateUsed(response, "posts.html")
